6e6f9342929f3d12bd63959c12a24900ad074b8b
Wesley Rou <wesleyrou@gmail.com>
2020-06-15 09:26:25 -0700

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return  ( 
+    <header>
+        <div id="img">
+            <img src="https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg" alt="Sally Student" />
+        </div>
+        <h1 class="title">About me</h1>
+    </header>
+    )
+  }
+
+  export default Header;
